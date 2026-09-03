79fd7799355ad2e65380c6912971b8830a983497
Rohit Ware <rohitware99@gmail.com>
2026-09-04 00:35:58 +0530

    add controller service and dependency injection

---
+++ src/main/java/com/rohit/springboot_learning/service/UserService.java
@@ -0,0 +1,11 @@
+package com.rohit.springboot_learning.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+    public void getUser() {
+        System.out.println("Getting user.....");
+    }
+
+}
