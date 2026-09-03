3595eba03bad8e7bd5dad774ab938acbea1e3dec
Rohit Ware <rohitware99@gmail.com>
2026-09-04 00:58:31 +0530

    add base request mapping for user controller

--- src/main/java/com/rohit/springboot_learning/controller/UserController.java
+++ src/main/java/com/rohit/springboot_learning/controller/UserController.java
@@ -3,9 +3,11 @@ package com.rohit.springboot_learning.controller;
 import com.rohit.springboot_learning.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
