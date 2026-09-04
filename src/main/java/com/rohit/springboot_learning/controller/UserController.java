ccff06027f267252d68b72774e8788343387fd6d
Rohit Ware <rohitware99@gmail.com>
2026-09-05 00:15:00 +0530

    add path variable endpoint for user

--- src/main/java/com/rohit/springboot_learning/controller/UserController.java
+++ src/main/java/com/rohit/springboot_learning/controller/UserController.java
@@ -3,6 +3,7 @@ package com.rohit.springboot_learning.controller;
 import com.rohit.springboot_learning.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class UserController {
     public String displayUser() {
         userService.getUser();
         return "User fetched successfully";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable int id) {
+        return "User ID" + id;
     }
 }
