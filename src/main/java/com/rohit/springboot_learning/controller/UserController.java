ecd55f88163d59229f370be1f98aa820a1d0d761
Rohit Ware <rohitware99@gmail.com>
2026-09-05 01:35:56 +0530

    add user model and creation endpoint

--- src/main/java/com/rohit/springboot_learning/controller/UserController.java
+++ src/main/java/com/rohit/springboot_learning/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.rohit.springboot_learning.controller;
 
 import com.rohit.springboot_learning.service.UserService;
+import com.rohit.springboot_learning.model.User;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/users")
@@ -34,4 +39,12 @@ public class UserController {
         return "Searching user: " + name;
     }
 
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+
+        String response = "User created: " + user.getName()
+                + ", Age: " + user.getAge();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
