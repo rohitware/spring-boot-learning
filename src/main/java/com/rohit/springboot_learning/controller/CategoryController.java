04115825cc534a52f4678933f574c0b091d70f3d
Rohit Ware <rohitware99@gmail.com>
2026-09-18 00:46:46 +0530

    Add category get by id endpoint

--- src/main/java/com/rohit/springboot_learning/controller/CategoryController.java
+++ src/main/java/com/rohit/springboot_learning/controller/CategoryController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/categories")
@@ -27,6 +29,15 @@ public class CategoryController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedCategory);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        Category category = categoryService.getCategoryById(id);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
     }
 
 }
--- src/main/java/com/rohit/springboot_learning/service/CategoryService.java
+++ src/main/java/com/rohit/springboot_learning/service/CategoryService.java
@@ -16,4 +16,8 @@ public class CategoryService {
     public Category createCategory(Category category) {
         return categoryRepository.save(category);
     }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
 }
